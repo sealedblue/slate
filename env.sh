facbce0a278cce9f797d8bd95de830f2358aacde
desiato <hbdesiato@gmx.de>
2026-07-26 11:16:38 +0200

    use build scripts from silverblue-nvidia

--- env.sh
+++ env.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 set -euo pipefail
+MAIN_BRANCH=main
 SIGSTORE_PUB=$(echo keys/*.pub)
 SIGSTORE_PREFIX=${SIGSTORE_PUB%.*}
 [ ${GITHUB_REPOSITORY-} ] && IMAGE_PREFIX=ghcr.io/${GITHUB_REPOSITORY%/*}
 IMAGE_NAME="$(basename $PWD)"
 TAG="$(git branch --show-current)"
-BASE_IMAGE="${IMAGE_PREFIX}/silverblue-nvidia:${TAG}-unsealed"
 IMAGE="${IMAGE_PREFIX}/${IMAGE_NAME}:${TAG}-unsealed"
+MAIN_IMAGE="${IMAGE_PREFIX}/${IMAGE_NAME}:${MAIN_BRANCH}-unsealed"
+DIGEST_NAME=$(systemd-escape "$IMAGE")
+
+BASE_IMAGE="${IMAGE_PREFIX}/silverblue-nvidia:${TAG}-unsealed"
--- update.sh
+++ update.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
 set -euxo pipefail
-CURRENT_BRANCH=$(git branch --show-current)
-git remote set-head origin -a
-MAIN_BRANCH="$(git symbolic-ref refs/remotes/origin/HEAD | cut -d/ -f4)"
+. ./env.sh
+CURRENT_BRANCH="$TAG"
 git switch "$MAIN_BRANCH"
 . ./env.sh
-MAIN_IMAGE="$IMAGE"
 
 SKIP=true
 ./check.sh || SKIP=false
@@ -17,14 +15,19 @@ BRANCHES=$(git ls-remote -qb | sed 's|.*\srefs/heads/||')
 for BRANCH in $BRANCHES; do
     [ "$BRANCH" != "$MAIN_BRANCH" ] || continue
     git fetch --depth=1 origin "refs/heads/$BRANCH:remotes/origin/$BRANCH"
-    git switch -c "$BRANCH" "origin/$BRANCH"
+    git switch "$BRANCH" || git switch -c "$BRANCH" "origin/$BRANCH"
     . ./env.sh
     if [ -e "diverge-$BRANCH" ]; then
         ./check.sh || ./build.sh
     else
         if ! $SKIP; then
-            podman tag "$MAIN_IMAGE" "$IMAGE"
-            ./push.sh
+            skopeo copy --sign-by-sigstore-private-key "${SIGSTORE_PREFIX}.private" \
+                --sign-passphrase-file "${SIGSTORE_PREFIX}.passphrase" \
+                --digestfile "${DIGEST_NAME}.digest" \
+                "docker://${MAIN_IMAGE}" "docker://${IMAGE}"
+            git add "${DIGEST_NAME}.digest"
+            git commit -m "${IMAGE} pushed" || true
+            git push
         fi
     fi
 done
